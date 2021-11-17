6f54c7e2509c61e242af0e4708844c15b092326e
Peter Domingues <peter@eridanusltda.com.br>
2021-11-17 13:48:46 -0300

    Todo list

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-10">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet risus fringilla, ultrices dui sit amet, bibendum felis. Suspendisse potenti. Mauris maximus sit amet elit sit amet interdum. Pellentesque at metus quis magna vestibulum consequat quis id orci. Nulla massa neque, ornare eu nulla mattis, finibus imperdiet est. Ut eu pellentesque ligula, vel cursus sapien. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque eu metus non nisi sagittis tincidunt. Mauris gravida porttitor velit ut sodales. Fusce eu sollicitudin nulla. Nunc sed lorem ut nunc accumsan imperdiet ac sed dolor. Ut porttitor et nisi eu eleifend. Fusce finibus quis lorem id rutrum. Vivamus ut posuere nulla. Duis vitae pellentesque metus. Maecenas hendrerit eros sit amet ipsum pharetra mollis.
+    </p>
+    <p>
+    Quisque et efficitur erat. Etiam vestibulum urna ac rhoncus lobortis. Etiam at urna eleifend enim placerat sollicitudin. Praesent gravida pulvinar tortor, eu fermentum orci rutrum sit amet. Duis sodales, leo id feugiat ornare, enim quam volutpat sapien, eget vulputate magna elit at est. Nunc scelerisque elit ex, et ultrices nunc auctor auctor. Phasellus ornare feugiat libero, et convallis mi sollicitudin maximus. Phasellus maximus leo urna, a cursus libero sodales vel. In at nisi pretium, ultrices nulla vel, tristique ex. Proin massa nulla, ultricies eget quam eget, tempor porttitor massa. Morbi mattis posuere urna ac dignissim. Quisque mi quam, dictum vel nisl id, molestie venenatis enim. Ut a ipsum dignissim, ornare nunc in, aliquet metus.
+    </p>
+    <p>
+    Vestibulum dictum condimentum lacus, id rhoncus nulla fringilla lacinia. Ut tempor mauris vel orci luctus ornare. Sed at imperdiet quam. Donec rhoncus sit amet turpis vitae aliquet. Fusce posuere, dui eget efficitur pellentesque, neque leo posuere nisi, non fringilla sem ipsum vitae diam. Integer id lectus id augue lacinia sodales. Proin bibendum ut turpis non pellentesque. Pellentesque porttitor elit at nisi consequat, eget suscipit justo mollis. Aenean eleifend, massa sit amet sagittis feugiat, tellus nunc rhoncus lectus, non eleifend risus orci et dui. In lacinia dapibus nulla tincidunt consequat.
+    </p>
   </div>
 </template>
